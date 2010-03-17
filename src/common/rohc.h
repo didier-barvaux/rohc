5e7c638bbe1ac4424502768ae0d147de86670d29
Didier Barvaux <didier.barvaux@toulouse.viveris.com>
2010-03-17 14:35:31 +0100

    License clarifications:  - State in REDAME and AUTHORS that license is GPL2+, ie. GPL version 2    or (at your option) any later version. This change is done because    the project from which the ROHC library was forked was under GPL2+.  - Add license statements at the beginning of every source file.

--- src/common/rohc.h
+++ src/common/rohc.h
@@ -1,3 +1,19 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 /**
  * @file rohc.h
  * @brief ROHC common definitions and routines 
@@ -47,7 +63,7 @@
  * <h2>License</h2>
  * 
  * <p>
- * The project is licensed under GPL, see the <a
+ * The project is licensed under GPL2+, see the <a
  * href="http://bazaar.launchpad.net/%7Edidier-barvaux/rohc/main/annotate/head%3A/COPYING">
  * COPYING</a> and <a
  * href="http://bazaar.launchpad.net/%7Edidier-barvaux/rohc/main/annotate/head%3A/AUTHORS">
