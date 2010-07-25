a7732a2d4d93fe22af315d560b96bd231003e38e
Didier Barvaux <didier@barvaux.org>
2010-07-25 15:42:14 +0200

    Add new application and script to generate ROHC compression statistics.
    The new application is better than the old scripts because the old scripts
    parse the library debug messages that may change from time to time and break
    the the statistics scripts. The new application uses the new API function
    rohc_comp_get_last_packet_info().

---
+++ statistics/generate_statistics.c
@@ -0,0 +1,421 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * @file   generate_statistics.c
+ * @brief  ROHC statistics program
+ * @author Didier Barvaux <didier@barvaux.org>
+ *
+ * The program takes a flow of IP packets as input (in the PCAP format) and
+ * generate some ROHC compression statistics with them.
+ */
+
+/* system includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <net/ethernet.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/ip6.h>
+
+/* include for the PCAP library */
+#include <pcap.h>
+
+/* ROHC includes */
+#include <rohc.h>
+#include <rohc_comp.h>
+
+
+/** The maximal size for the ROHC packets */
+#define MAX_ROHC_SIZE  (5 * 1024)
+
+/** The length of the Linux Cooked Sockets header */
+#define LINUX_COOKED_HDR_LEN  16
+
+/** The minimum Ethernet length (in bytes) */
+#define ETHER_FRAME_MIN_LEN 60
+
+
+/* prototypes of private functions */
+static void usage(void);
+static int generate_comp_stats_all(const int use_large_cid,
+                                   const unsigned int max_contexts,
+                                   const char *filename);
+static int generate_comp_stats_one(struct rohc_comp *comp,
+                                   const unsigned long num_packet,
+                                   const struct pcap_pkthdr header,
+                                   const unsigned char *packet,
+                                   const int link_len);
+
+
+/**
+ * @brief Main function for the ROHC statistics program
+ *
+ * @param argc  The number of program arguments
+ * @param argv  The program arguments
+ * @return      The unix return code:
+ *               \li 0 in case of success,
+ *               \li 1 in case of failure
+ */
+int main(int argc, char *argv[])
+{
+	char *cid_type = NULL;
+	char *source_filename = NULL;
+	int status = 1;
+	int max_contexts = 15;
+	int use_large_cid;
+	int args_used;
+
+	/* parse program arguments, print the help message in case of failure */
+	if(argc <= 1)
+	{
+		usage();
+		goto error;
+	}
+
+	for(argc--, argv++; argc > 0; argc -= args_used, argv += args_used)
+	{
+		args_used = 1;
+
+		if(!strcmp(*argv, "-h"))
+		{
+			/* print help */
+			usage();
+			goto error;
+		}
+		else if(!strcmp(*argv, "--max-contexts"))
+		{
+			/* get the maximum number of contexts the test should use */
+			max_contexts = atoi(argv[1]);
+			args_used++;
+		}
+		else if(cid_type == NULL)
+		{
+			/* get the type of CID to use within the ROHC library */
+			cid_type = argv[0];
+		}
+		else if(source_filename == NULL)
+		{
+			/* get the name of the file that contains the packets to compress */
+			source_filename = argv[0];
+		}
+		else
+		{
+			/* do not accept more than one filename without option name */
+			usage();
+			goto error;
+		}
+	}
+
+	/* the maximum number of ROHC contexts should be valid */
+	if(max_contexts < 1 || max_contexts > 16384)
+	{
+		fprintf(stderr, "the maximum number of ROHC contexts should be "
+		        "between 1 and 16384\n\n");
+		usage();
+		goto error;
+	}
+
+	/* check CID type */
+	if(!strcmp(cid_type, "smallcid"))
+	{
+		use_large_cid = 0;
+	}
+	else if(!strcmp(cid_type, "largecid"))
+	{
+		use_large_cid = 1;
+	}
+	else
+	{
+		fprintf(stderr, "invalid CID type '%s', only 'smallcid' and 'largecid' "
+		       "expected\n", cid_type);
+		usage();
+		goto error;
+	}
+
+	/* the source filename is mandatory */
+	if(source_filename == NULL)
+	{
+		fprintf(stderr, "source filename is mandatory\n");
+		usage();
+		goto error;
+	}
+
+	/* init the CRC tables */
+	crc_init_table(crc_table_3, crc_get_polynom(CRC_TYPE_3));
+	crc_init_table(crc_table_7, crc_get_polynom(CRC_TYPE_7));
+	crc_init_table(crc_table_8, crc_get_polynom(CRC_TYPE_8));
+
+	/* generate ROHC compression statistics with the packets from the file */
+	status = generate_comp_stats_all(use_large_cid, max_contexts, source_filename);
+
+error:
+	return status;
+}
+
+
+/**
+ * @brief Print usage of the performance test application
+ */
+static void usage(void)
+{
+	fprintf(stderr,
+	        "ROHC statistics tool: generate ROHC compression statistics\n"
+	        "with a flow of IP packets\n"
+	        "\n"
+	        "usage: generate_statistics [OPTIONS] CID_TYPE FLOW\n"
+	        "\n"
+	        "with:\n"
+	        "  CID_TYPE                The type of CID to use among 'smallcid'\n"
+	        "                          and 'largecid'\n"
+	        "  FLOW                    The flow of Ethernet frames to compress\n"
+	        "                          (in PCAP format)\n"
+	        "\n"
+	        "options:\n"
+	        "  -h                      Print this usage and exit\n"
+	        "  --max-contexts NUM      The maximum number of ROHC contexts to\n"
+	        "                          simultaneously use during the test\n");
+}
+
+
+/**
+ * @brief Generate ROHC compression statistics with a flow of IP packets
+ *
+ * @param use_large_cid  Whether the compressor shall use large CIDs
+ * @param max_contexts   The maximum number of ROHC contexts to use
+ * @param filename       The name of the PCAP file that contains the IP packets
+ * @return               0 in case of success,
+ *                       1 in case of failure
+ */
+static int generate_comp_stats_all(const int use_large_cid,
+                                   const unsigned int max_contexts,
+                                   const char *filename)
+{
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t *handle;
+	int link_layer_type;
+	int link_len;
+
+	struct rohc_comp *comp;
+
+	unsigned long num_packet;
+	struct pcap_pkthdr header;
+	unsigned char *packet;
+
+	int is_failure = 1;
+
+	/* open the source PCAP file */
+	handle = pcap_open_offline(filename, errbuf);
+	if(handle == NULL)
+	{
+		fprintf(stderr, "failed to open the source pcap file: %s\n", errbuf);
+		goto error;
+	}
+
+	/* link layer in the source PCAP file must be Ethernet */
+	link_layer_type = pcap_datalink(handle);
+	if(link_layer_type != DLT_EN10MB &&
+	   link_layer_type != DLT_LINUX_SLL &&
+	   link_layer_type != DLT_RAW)
+	{
+		fprintf(stderr, "link layer type %d not supported in source PCAP file "
+		        "(supported = %d, %d, %d)\n", link_layer_type, DLT_EN10MB,
+		        DLT_LINUX_SLL, DLT_RAW);
+		goto close_input;
+	}
+
+	/* determine the size of the link layer header */
+	if(link_layer_type == DLT_EN10MB)
+		link_len = ETHER_HDR_LEN;
+	else if(link_layer_type == DLT_LINUX_SLL)
+		link_len = LINUX_COOKED_HDR_LEN;
+	else /* DLT_RAW */
+		link_len = 0;
+
+	/* create the ROHC compressor */
+	comp = rohc_alloc_compressor(max_contexts - 1, 0, 0, 0);
+	if(comp == NULL)
+	{
+		fprintf(stderr, "cannot create the ROHC compressor\n");
+		goto close_input;
+	}
+	rohc_activate_profile(comp, ROHC_PROFILE_UNCOMPRESSED);
+	rohc_activate_profile(comp, ROHC_PROFILE_UDP);
+	rohc_activate_profile(comp, ROHC_PROFILE_IP);
+	rohc_activate_profile(comp, ROHC_PROFILE_UDPLITE);
+	rohc_activate_profile(comp, ROHC_PROFILE_RTP);
+	rohc_c_set_large_cid(comp, use_large_cid);
+
+	/* output the statistics columns names */
+	printf("STAT\t"
+	       "\"packet number\"\t"
+	       "\"context mode\"\t"
+	       "\"context mode (string)\"\t"
+	       "\"context state\"\t"
+	       "\"context state (string)\"\t"
+	       "\"packet type\"\t"
+	       "\"packet type (string)\"\t"
+	       "\"uncompressed packet size (bytes)\"\t"
+	       "\"uncompressed header size (bytes)\"\t"
+	       "\"compressed packet size (bytes)\"\t"
+	       "\"compressed header size (bytes)\"\n");
+	fflush(stdout);
+
+	/* for each packet extracted from the PCAP file */
+	num_packet = 0;
+	while((packet = (unsigned char *) pcap_next(handle, &header)) != NULL)
+	{
+		int ret;
+
+		num_packet++;
+
+		/* compress the packet and generate statistics */
+		ret = generate_comp_stats_one(comp, num_packet, header, packet, link_len);
+		if(ret != 0)
+		{
+			fprintf(stderr, "packet %lu: failed to compress or generate stats "
+			        "for packet\n", num_packet);
+			goto destroy_comp;
+		}
+	}
+
+	/* everything went fine */
+	is_failure = 0;
+
+destroy_comp:
+	rohc_free_compressor(comp);
+close_input:
+	pcap_close(handle);
+error:
+	return is_failure;
+}
+
+
+/**
+ * @brief Compress and decompress one uncompressed IP packet with the given
+ *        compressor and decompressor
+ *
+ * @param comp        The compressor to use to compress the IP packet
+ * @param num_packet  A number affected to the IP packet to compress
+ * @param header      The PCAP header for the packet
+ * @param packet      The packet to compress (link layer included)
+ * @param link_len    The length of the link layer header before IP data
+ * @return            0 in case of success,
+ *                    1 in case of failure
+ */
+static int generate_comp_stats_one(struct rohc_comp *comp,
+                                   const unsigned long num_packet,
+                                   const struct pcap_pkthdr header,
+                                   const unsigned char *packet,
+                                   const int link_len)
+{
+	const unsigned char *ip_packet;
+	int ip_size;
+	static unsigned char rohc_packet[MAX_ROHC_SIZE];
+	int rohc_size;
+	rohc_comp_last_packet_info_t last_packet_info;
+	const char *modes[] = { "error", "U-mode", "O-mode", "R-mode" };
+	const char *states[] = { "error", "IR", "FO", "SO" };
+	const char *packet_types[] =
+	{
+		"IR", "IR-DYN",
+		"UO-0",
+		"UO-1", "UO-1-ID", "UO-1-TS", "UO-1-RTP",
+		"UOR-2", "UOR-2-RTP", "UOR-2-ID", "UOR-2-TS",
+		"CCE", "CCE(off)",
+		"Normal",
+		"unknown"
+	};
+	int ret;
+
+	/* check frame length */
+	if(header.len <= link_len || header.len != header.caplen)
+	{
+		fprintf(stderr, "packet #%lu: bad PCAP packet (len = %d, caplen = %d)\n",
+		       num_packet, header.len, header.caplen);
+		goto error;
+	}
+
+	/* skip the link layer header */
+	ip_packet = packet + link_len;
+	ip_size = header.len - link_len;
+
+	/* check for padding after the IP packet in the Ethernet payload */
+	if(link_len == ETHER_HDR_LEN && header.len == ETHER_FRAME_MIN_LEN)
+	{
+		int version;
+		int tot_len;
+
+		version = (ip_packet[0] >> 4) & 0x0f;
+
+		if(version == 4)
+		{
+			struct iphdr *ip = (struct iphdr *) ip_packet;
+			tot_len = ntohs(ip->tot_len);
+		}
+		else
+		{
+			struct ip6_hdr *ip = (struct ip6_hdr *) ip_packet;
+			tot_len = sizeof(struct ip6_hdr) + ntohs(ip->ip6_plen);
+		}
+
+		if(tot_len < ip_size)
+		{
+			/* the Ethernet frame has some bytes of padding after the IP packet */
+			ip_size = tot_len;
+		}
+	}
+
+	/* compress the IP packet */
+	rohc_size = rohc_compress(comp, (unsigned char *) ip_packet, ip_size,
+	                          rohc_packet, MAX_ROHC_SIZE);
+	if(rohc_size <= 0)
+	{
+		fprintf(stderr, "packet #%lu: compression failed\n", num_packet);
+		goto error;
+	}
+
+	/* get some statistics about the last compressed packet */
+	ret = rohc_comp_get_last_packet_info(comp, &last_packet_info);
+	if(ret != ROHC_OK)
+	{
+		fprintf(stderr, "packet #%lu: cannot get stats about the last compressed "
+		        "packet\n", num_packet);
+		goto error;
+	}
+
+	/* output some statistics about the last compressed packet */
+	printf("STAT\t%lu\t%d\t%s\t%d\t%s\t%d\t%s\t%lu\t%lu\t%lu\t%lu\n",
+	       num_packet,
+	       last_packet_info.context_mode,
+	       modes[last_packet_info.context_mode],
+	       last_packet_info.context_state,
+	       states[last_packet_info.context_state],
+	       last_packet_info.packet_type,
+	       packet_types[last_packet_info.packet_type],
+	       last_packet_info.total_last_uncomp_size,
+	       last_packet_info.header_last_uncomp_size,
+	       last_packet_info.total_last_comp_size,
+	       last_packet_info.header_last_comp_size);
+	fflush(stdout);
+
+	return 0;
+
+error:
+	return 1;
+}
+
---
+++ statistics/generate_statistics.sh
@@ -0,0 +1,270 @@
+#!/bin/sh
+#
+# file:        generate_statistics.sh
+# description: Generate some cool graphs about ROHC library compression
+# author:      Didier Barvaux <didier@barvaux.org>
+#
+
+# determine the base directory and the application path
+SCRIPT="$0"
+VERBOSE="$1"
+if [ "x$MAKELEVEL" != "x" ] ; then
+	BASEDIR="${srcdir}"
+	APP="./generate_statistics"
+else
+	BASEDIR=$( dirname "${SCRIPT}" )
+	APP="${BASEDIR}/generate_statistics"
+fi
+
+# generate statistics for all captures found in the samples/ subdirectory
+for CAPTURE in $(find "${BASEDIR}/samples/" -name source.pcap | sort) ; do
+
+	# determine the name of the stream stored in the capture
+	STREAM="./$(dirname ${CAPTURE} | sed -e "s|${BASEDIR}/samples/||")"
+
+	# check that stream name is not empty
+	if [ -z "${STREAM}" ] ; then
+		echo "empty stream name for capture ${CAPTURE} !"
+		exit 1
+	fi
+
+	# run the statistics application and retrieve its output
+	OUTPUT=$( ${APP} smallcid ${CAPTURE} 2>/dev/null )
+	if [ $? -ne 0 ] ; then
+		echo "compression failed for capture ${CAPTURE} !"
+		exit 1
+	fi
+
+	# determine filename for raw output
+	RAW_OUTPUT="${BASEDIR}/html/${STREAM}/raw.txt"
+
+	# determine type of packet in capture
+	PACKET_TYPE=$(echo $STREAM | sed -e 's|^./||' | sed -e 's/_/\//g' | tr '[a-z]' '[A-Z]')
+
+	if [ "${VERBOSE}" = "verbose" ] ; then
+		echo "generate statistics for ${PACKET_TYPE} packets:"
+	else
+		echo "generate statistics for ${PACKET_TYPE} packets"
+	fi
+
+	# create the output directory
+	mkdir -p "${BASEDIR}/html/${STREAM}" || exit 1
+
+	# create the raw output
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\trun statistics application... "
+	echo -e "${OUTPUT}" | grep "^STAT" > ${RAW_OUTPUT} || exit 1
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create the graph with context modes
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate graph with context modes... "
+	echo -e "set terminal png\n" \
+	        "set output '${BASEDIR}/html/${STREAM}/modes.png'\n" \
+	        "set title 'Compression modes for ${PACKET_TYPE} packets'\n" \
+	        "set xlabel 'packet number in capture'\n" \
+	        "plot [] [0:4] '${RAW_OUTPUT}' using 2:3:yticlabels(4) title columnhead(3)" \
+		| gnuplot 2>/dev/null \
+		|| exit 1
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create the graph with context states
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate graph with context states... "
+	echo -e "set terminal png\n" \
+	        "set output '${BASEDIR}/html/${STREAM}/states.png'\n" \
+	        "set title 'Compression states for ${PACKET_TYPE} packets'\n" \
+	        "set xlabel 'packet number in capture'\n" \
+	        "plot [] [0:4] '${RAW_OUTPUT}' using 2:5:yticlabels(6) title columnhead(5)" \
+		| gnuplot 2>/dev/null \
+		|| exit 1
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create the graph with packet types
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate graph with packet types... "
+	echo -e "set terminal png\n" \
+	        "set output '${BASEDIR}/html/${STREAM}/packet_types.png'\n" \
+	        "set title 'ROHC packet types for ${PACKET_TYPE} packets'\n" \
+	        "set xlabel 'packet number in capture'\n" \
+	        "plot [] [-1:14] '${RAW_OUTPUT}' using 2:7:yticlabels(8) title columnhead(7)" \
+		| gnuplot 2>/dev/null \
+		|| exit 1
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create the graph with (un)compressed packet sizes
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate graph with (un)compressed packet sizes... "
+	echo -e "set terminal png\n" \
+	        "set output '${BASEDIR}/html/${STREAM}/packet_sizes.png'\n" \
+	        "set title 'Packet sizes for compression of ${PACKET_TYPE} packets'\n" \
+	        "set xlabel 'packet number in capture'\n" \
+	        "set ylabel 'packet size (bytes)'\n" \
+	        "plot '${RAW_OUTPUT}' using 2:9 title columnhead(9) with lines," \
+	        "     '${RAW_OUTPUT}' using 2:11 title columnhead(11) with lines" \
+		| gnuplot 2>/dev/null \
+		|| exit 1
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create the graph with (un)compressed header sizes
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate graph with (un)compressed header sizes... "
+	echo -e "set terminal png\n" \
+	        "set output '${BASEDIR}/html/${STREAM}/header_sizes.png'\n" \
+	        "set title 'Header sizes for compression of ${PACKET_TYPE} packets'\n" \
+	        "set xlabel 'packet number in capture'\n" \
+	        "set ylabel 'header size (bytes)'\n" \
+	        "plot '${RAW_OUTPUT}' using 2:10 title columnhead(10) with lines," \
+	        "     '${RAW_OUTPUT}' using 2:12 title columnhead(12) with lines" \
+		| gnuplot 2>/dev/null \
+		|| exit 1
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create context mode counters
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate context mode counters... "
+	for CONTEXT_MODE in "U-mode" "O-mode" "R-mode" ; do
+		gawk "\$4 == \"${CONTEXT_MODE}\" { print \$11 }" \
+		     "${RAW_OUTPUT}" \
+			| wc -l > ${BASEDIR}/html/${STREAM}/context_mode_${CONTEXT_MODE}.count \
+			|| exit 1
+	done
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create context state counters
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate context state counters... "
+	for CONTEXT_STATE in "IR" "FO" "SO" ; do
+		gawk "\$6 == \"${CONTEXT_STATE}\" { print \$11 }" \
+		     "${RAW_OUTPUT}" \
+			| wc -l > ${BASEDIR}/html/${STREAM}/context_state_${CONTEXT_STATE}.count \
+			|| exit 1
+	done
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create packet type counters
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate packet type counters... "
+	for PACKET_TYPE in "IR" "IR-DYN" \
+	                   "UO-0" \
+	                   "UO-1" "UO-1-ID" "UO-1-TS" "UO-1-RTP" \
+	                   "UOR-2" "UOR-2-RTP" "UOR-2-ID" "UOR-2-TS" \
+	                   "CCE" "CCE(off)" \
+	                   "Normal"
+	do
+		gawk "\$8 == \"${PACKET_TYPE}\" { print \$11 }" \
+		     "${RAW_OUTPUT}" \
+			| wc -l > ${BASEDIR}/html/${STREAM}/packet_type_${PACKET_TYPE}.count \
+			|| exit 1
+	done
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create packet compression gain
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate packet compression gain... "
+	gawk 'BEGIN { uncomp=0 ; comp=0 }\
+	      $2 != "\"packet" { uncomp+=$9 ; comp+=$11 }\
+	      END { printf "scale=2\n100-" comp "*100/" uncomp "\n" }' \
+	     "${RAW_OUTPUT}" \
+		| bc > ${BASEDIR}/html/${STREAM}/packet_compression.gain \
+		|| exit 1
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+	# create header compression gain
+	[ "${VERBOSE}" = "verbose" ] && echo -ne "\tcreate header compression gain... "
+	gawk 'BEGIN { uncomp=0 ; comp=0 }\
+	      $2 != "\"packet" { uncomp+=$10 ; comp+=$12 }\
+	      END { printf "scale=2\n100-" comp "*100/" uncomp "\n" }' \
+	     "${RAW_OUTPUT}" \
+		| bc > ${BASEDIR}/html/${STREAM}/header_compression.gain \
+		|| exit 1
+	[ "${VERBOSE}" = "verbose" ] && echo "done"
+
+done
+
+# generate the HTML page which summarize all the results
+HTML_OUTPUT="${BASEDIR}/html/index.html"
+echo -n "" > ${HTML_OUTPUT}
+echo -e "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" >> ${HTML_OUTPUT}
+echo -e "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">" >> ${HTML_OUTPUT}
+echo -e "\t<head>" >> ${HTML_OUTPUT}
+echo -e "\t\t<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />" >> ${HTML_OUTPUT}
+echo -e "\t\t<title>ROHC compression statistics</title>" >> ${HTML_OUTPUT}
+echo -e "\t\t<style type=\"text/css\">" >> ${HTML_OUTPUT}
+echo -e "\t\t\tbody { font-size: small; }" >> ${HTML_OUTPUT}
+echo -e "\t\t\ttable, tr, th, td { border: solid thin black; border-collapse: collapse; }" >> ${HTML_OUTPUT}
+echo -e "\t\t\ttable { width: 95%; }" >> ${HTML_OUTPUT}
+echo -e "\t\t</style>" >> ${HTML_OUTPUT}
+echo -e "\t</head>" >> ${HTML_OUTPUT}
+echo -e "\t<body>" >> ${HTML_OUTPUT}
+
+echo -e "\t\t<h1>ROHC compression statistics</h1>" >> ${HTML_OUTPUT}
+
+echo -e "\t\t<table>" >> ${HTML_OUTPUT}
+
+echo -e "\t\t<tr>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"3\">Type of stream</th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th colspan=\"3\">Context modes</th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th colspan=\"3\">Context states</th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th colspan=\"14\">Packet types</th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th colspan=\"2\">Compression gain</th>" >> ${HTML_OUTPUT}
+echo -e "\t\t</tr>" >> ${HTML_OUTPUT}
+
+echo -e "\t\t<tr>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Unidirectional Mode\">U-Mode</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Bidirectional Optimistic Mode\">O-mode</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Bidirectional Reliable Mode\">R-mode</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Initialisation &amp; Refresh\">IR</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"First Order\">FO</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Second Order\">SO</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Initialisation &amp; Refresh\">IR</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Initialisation &amp; Refresh DYNamic\">IR-DYN</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Unidirectional/Optimistic packet type 0\">UO-0</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th colspan=\"4\"><acronym title=\"Unidirectional/Optimistic packet type 1\">UO-1</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th colspan=\"4\"><acronym title=\"Unidirectional/Optimistic/Reliable packet type 2\">UOR-2</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"CCE packet type for UDP-Lite profile\">CCE</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"CCE(off) packet type for UDP-Lite profile\">CCE(off)</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\"><acronym title=\"Normal packet type for Uncompressed profile\">Normal</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\">Packet</th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th rowspan=\"2\">Header</th>" >> ${HTML_OUTPUT}
+echo -e "\t\t</tr>" >> ${HTML_OUTPUT}
+
+echo -e "\t\t<tr>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th><acronym title=\"UO-1 for non-RTP profiles\">-</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th><acronym title=\"UO-1 with IP-ID bits\">ID</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th><acronym title=\"UO-1 with RTP TS bits\">TS</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th><acronym title=\"UO-1 for RTP profile\">RTP</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th><acronym title=\"UOR-2 for non-RTP profiles\">-</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th><acronym title=\"UOR-2 for RTP profile\">RTP</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th><acronym title=\"UOR-2 with IP-ID bits\">ID</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t\t<th><acronym title=\"UOR-2 with TS bits\">TS</acronym></th>" >> ${HTML_OUTPUT}
+echo -e "\t\t</tr>" >> ${HTML_OUTPUT}
+
+for CAPTURE in $(find "${BASEDIR}/samples/" -name source.pcap | sort ) ; do
+
+	STREAM="./$(dirname ${CAPTURE} | sed -e "s|${BASEDIR}/samples/||")"
+	PACKET_TYPE=$(echo $STREAM | sed -e 's|^./||' | sed -e 's/_/\//g' | tr '[a-z]' '[A-Z]')
+
+	echo -e "\t\t<tr>" >> ${HTML_OUTPUT}
+	echo -e "\t\t\t<td>${PACKET_TYPE} packets</td>" >> ${HTML_OUTPUT}
+	for CONTEXT_MODE in "U-mode" "O-mode" "R-mode" ; do
+		echo -e "\t\t\t<td><a href=\"./${STREAM}/modes.png\">$(cat ${BASEDIR}/html/${STREAM}/context_mode_${CONTEXT_MODE}.count)</a></td>" >> ${HTML_OUTPUT}
+	done
+	for CONTEXT_STATE in "IR" "FO" "SO" ; do
+		echo -e "\t\t\t<td><a href=\"./${STREAM}/states.png\">$(cat ${BASEDIR}/html/${STREAM}/context_state_${CONTEXT_STATE}.count)</a></td>" >> ${HTML_OUTPUT}
+	done
+	for PACKET_TYPE in "IR" "IR-DYN" \
+	                   "UO-0" \
+	                   "UO-1" "UO-1-ID" "UO-1-TS" "UO-1-RTP" \
+	                   "UOR-2" "UOR-2-RTP" "UOR-2-ID" "UOR-2-TS" \
+	                   "CCE" "CCE(off)" \
+	                   "Normal"
+	do
+		echo -e "\t\t\t<td><a href=\"./${STREAM}/packet_types.png\">$(cat ${BASEDIR}/html/${STREAM}/packet_type_${PACKET_TYPE}.count)</a></td>" >> ${HTML_OUTPUT}
+	done
+	echo -e "\t\t\t<td><a href=\"./${STREAM}/packet_sizes.png\">$(cat ${BASEDIR}/html/${STREAM}/packet_compression.gain)&nbsp;%</a></td>" >> ${HTML_OUTPUT}
+	echo -e "\t\t\t<td><a href=\"./${STREAM}/header_sizes.png\">$(cat ${BASEDIR}/html/${STREAM}/header_compression.gain)&nbsp;%</a></td>" >> ${HTML_OUTPUT}
+	echo -e "\t\t</tr>" >> ${HTML_OUTPUT}
+
+done
+
+echo -e "\t\t</table>" >> ${HTML_OUTPUT}
+echo -e "\t</body>" >> ${HTML_OUTPUT}
+echo -e "</html>" >> ${HTML_OUTPUT}
+
+[ "${VERBOSE}" = "verbose" ] && echo ""
+echo "HTML summary was created for you in \"${HTML_OUTPUT}\"."
+[ "${VERBOSE}" = "verbose" ] && echo ""
+
+exit 0
+
