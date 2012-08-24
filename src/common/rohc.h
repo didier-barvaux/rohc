43795fe520a138682a5bb61b0274c23b24974058
Didier Barvaux <didier@barvaux.org>
2012-08-24 20:21:20 +0200

    Rework the main page of the doxygen documentation.

--- src/common/rohc.h
+++ src/common/rohc.h
@@ -28,19 +28,17 @@
 /**
  * \mainpage
  *
- *
  * <h2>Introduction</h2>
- *
- * <p>The ROHC library provides an easy and robust way for applications to
- * reduce their bandwidth usage on network links with limited capacity.
- * Headers of network packets are compressed with the ROHC protocol and
- * algorithms.</p>
- *
- * <p>RObust Header Compression (ROHC) is a set of standards defined by the
- * <a href="http://www.ietf.org/">IETF</a>. The ROHC library is a free,
- * opensource and efficient implementation of them. The list of features
- * implemented is available on a separate page: \ref features</p>
- *
+ * <p>The <a href="http://rohc-lib.org/" title="Official website">ROHC library</a>
+ * provides an easy and robust way for applications to reduce their bandwidth
+ * usage on network links with limited capacity.  Headers of network packets are
+ * compressed with the ROHC protocol and algorithms.</p>
+ * <p><a href="http://rohc-lib.org/wiki/doku.php?id=rohc-protocol"
+ * title="An introduction to the ROHC protocol">RObust Header Compression
+ * (ROHC)</a> is a set of standards defined by the <a href="http://www.ietf.org/"
+ * title="The IETF website">IETF</a>. The ROHC library is a free, opensource and
+ * efficient implementation of them. The list of features implemented is available
+ * on a separate page: \ref features</p>
  * <ul>
  *   <li>Official website: <a href="http://rohc-lib.org/">
  *                          http://rohc-lib.org/</a></li>
@@ -54,17 +52,13 @@
  *                    http://bugs.launchpad.net/rohc</a></li>
  * </ul>
  *
- *
  * <h2>License</h2>
- *
  * <p>The project is licensed under GPL2+.
  * See the <a href="http://bazaar.launchpad.net/%7Edidier-barvaux/rohc/main/annotate/head%3A/COPYING">COPYING</a>
  * and <a href="http://bazaar.launchpad.net/%7Edidier-barvaux/rohc/main/annotate/head%3A/AUTHORS">AUTHORS</a>
  * files for more details.</p>
  *
- *
  * <h2>Libraries</h2>
- *
  * <p>
  * The sources are in the src subdirectory. The sources are separated into
  * three libraries:
@@ -75,10 +69,8 @@
  *   <li>a library that handles the decompression process</li>
  *  </ul>
  * </p>
- *
  * <p>See the <a href="http://bazaar.launchpad.net/%7Edidier-barvaux/rohc/main/annotate/head%3A/INSTALL"> INSTALL file</a>
  * to learn to build the libraries.</p>
- *
  *
  * <h2>API documentation</h2>
  * <p>The APIs for ROHC common, compression and decompression are available on
@@ -90,100 +82,29 @@
  *  </ul>
  * </p>
  *
- *
- * <h2>Non-regression tests</h2>
- *
+ * <h2>Tests</h2>
  * <p>
- * The test subdirectory contains several test applications. See the <a href="http://bazaar.launchpad.net/%7Edidier-barvaux/rohc/main/annotate/head%3A/INSTALL">INSTALL file</a> to learn how to use these tools.
+ * Several tests may be run to check the library behaviour. See the
+ * <a href="http://bazaar.launchpad.net/%7Edidier-barvaux/rohc/main/annotate/head%3A/INSTALL">INSTALL file</a> to learn how to use these tools.
  * </p>
  *
- *
  * <h2>References</h2>
- *
- * <table style="border-collapse: collapse; border: solid thin black;">
- *  <tr>
- *   <td>RFC 3095</td>
- *   <td>
- *    <p>
- *     RObust Header Compression (ROHC): Framework and four profiles: RTP,
- *     UDP, ESP, and uncompressed.<br>
- *      <a href="http://www.ietf.org/rfc/rfc3095.txt">
- *       http://www.ietf.org/rfc/rfc3095.txt</a>
- *    </p>
- *   </td>
- *  </tr>
- *  <tr>
- *   <td>RFC 3096</td>
- *   <td>
- *    <p>
- *     Requirements for robust IP/UDP/RTP header compression.<br>
- *      <a href="http://www.ietf.org/rfc/rfc3096.txt">
- *       http://www.ietf.org/rfc/rfc3096.txt</a>
- *    </p>
- *   </td>
- *  </tr>
- *  <tr>
- *   <td>RFC 3828</td>
- *   <td>
- *    <p>
- *     The Lightweight User Datagram Protocol (UDP-Lite).<br>
- *     <a href="http://www.ietf.org/rfc/rfc3828.txt">
- *     http://www.ietf.org/rfc/rfc3828.txt</a>
- *    </p>
- *   </td>
- *  </tr>
- *  <tr>
- *   <td>RFC 3843
- *   <td>
- *    <p>
- *     RObust Header Compression (ROHC): A Compression Profile for IP.<br>
- *     <a href="http://www.ietf.org/rfc/rfc3843.txt">
- *     http://www.ietf.org/rfc/rfc3843.txt</a>
- *    </p>
- *   </td>
- *  </tr>
- *  <tr>
- *   <td>RFC 4019</td>
- *   <td>
- *    <p>
- *     RObust Header Compression (ROHC): Profiles for User Datagram Protocol
- *     (UDP) Lite.<br>
- *     <a href="http://www.ietf.org/rfc/rfc4019.txt">
- *     http://www.ietf.org/rfc/rfc4019.txt</a>
- *    </p>
- *   </td>
- *  </tr>
- *  <tr>
- *   <td>ROHC library</td>
- *   <td>
- *    <p>
- *     The Open Source ROHC library described by the documentation you are
- *     currently reading.<br>
- *     <a href="http://launchpad.net/rohc">http://launchpad.net/rohc</a>
- *    </p>
- *   </td>
- *  </tr>
- *  <tr>
- *   <td>ROHC Linux</td>
- *   <td>
- *    <p>
- *     A GPL-licensed implementation of ROHC over PPP for the 2.4 Linux
- *     kernel. The ROHC library is mainly based on this software.<br>
- *     <a href="http://rohc.sourceforge.net/">http://rohc.sourceforge.net/</a>
- *    </p>
- *   </td>
- *  </tr>
- *  <tr>
- *   <td>UDP-Lite</td>
- *   <td>
- *    <p>
- *     An UDP-Lite implementation for the Linux kernel.<br>
- *     <a href="http://www.erg.abdn.ac.uk/users/gerrit/udp-lite/">
- *     http://www.erg.abdn.ac.uk/users/gerrit/udp-lite/</a>
- *    </p>
- *   </td>
- *  </tr>
- * </table>
+ * <dl style="padding-bottom: 1em;">
+ *  <dt><a href="http://tools.ietf.org/html/rfc3095">RFC&nbsp;3095</a></dt>
+ *  <dd>ROHC: Framework and four profiles: RTP, UDP, ESP, and uncompressed.</dd>
+ *  <dt><a href="http://tools.ietf.org/html/rfc3096">RFC&nbsp;3096</a></dt>
+ *  <dd>Requirements for robust IP/UDP/RTP header compression.</dd>
+ *  <dt><a href="http://tools.ietf.org/html/rfc3843">RFC&nbsp;3843</a></dt>
+ *  <dd>ROHC: A Compression Profile for IP.</dd>
+ *  <dt><a href="http://tools.ietf.org/html/rfc4019">RFC&nbsp;4019</a></dt>
+ *  <dd>ROHC: Profiles for User Datagram Protocol (UDP) Lite.</dd>
+ *  <dt><a href="http://rohc-lib.org/">ROHC library</a></dt>
+ *  <dd>The Open Source ROHC library described by the documentation you are
+ *      currently reading.</dd>
+ *  <dt><a href="http://rohc.sourceforge.net/">ROHC Linux</a></dt>
+ *  <dd>A GPL-licensed implementation of ROHC over PPP for the 2.4 Linux kernel.
+ *      The ROHC library started as a fork of this project.</dd>
+ * </dl>
  */
 
 
