bd066ecd49ee671766c137ca2337834cd24c1dd4
Didier Barvaux <didier@barvaux.org>
2012-12-06 21:37:59 +0100

    Avoid s6_addr clash between src/common/protocols/ipv6.h and arpa/inet.h.

--- src/common/protocols/ipv6.h
+++ src/common/protocols/ipv6.h
@@ -40,13 +40,10 @@ struct ipv6_addr
 {
 	union
 	{
-		uint8_t __u6_addr8[16];
-		uint16_t __u6_addr16[8];
-		uint32_t __u6_addr32[4];
-	} __in6_u;
-#define s6_addr		__in6_u.__u6_addr8
-#define s6_addr16		__in6_u.__u6_addr16
-#define s6_addr32		__in6_u.__u6_addr32
+		uint8_t u8[16];
+		uint16_t u16[8];
+		uint32_t u32[4];
+	} addr;
 };
 
 
