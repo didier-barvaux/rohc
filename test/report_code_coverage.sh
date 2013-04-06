bff1d85d7d9d336df11116a8715a526a750f93a3
Didier Barvaux <didier@barvaux.org>
2013-04-06 10:18:15 +0200

    Fix bug #804565: Measure test coverage https://bugs.launchpad.net/rohc/+bug/804565
    Add configure option --enable-code-coverage to compute the code coverage
    of the library by the tests.

---
+++ test/report_code_coverage.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Create a report of code coverage with the help of zcov.
+#
+# Do not use this script directly, run configure with --enable-code-coverage,
+# build the library, then run the tests:
+#   ./configure --enable-code-coverage
+#   make clean
+#   make all
+#   make check
+#
+# note: LANG=C and LC_ALL=C are required for zcov to work correctly
+
+echo "" >&2
+
+# scan for gcov output files, create the output.zcov report file
+echo -n "Collect information about code coverage... " >&2
+LANG=C LC_ALL=C zcov-scan output.zcov . || exit 1
+echo "done." >&2
+
+# generate one HTML report from the collected data
+echo -n "Generate HTML report about code coverage... " >&2
+LANG=C LC_ALL=C zcov-genhtml output.zcov coverage-report/ || exit 1
+echo "done." >&2
+
+echo "" >&2
+echo "Load ./coverage-report/index.html in your favorite browser." >&2
+echo "" >&2
+
