5822a2128102692af784639d7e20158e2e99b281
Didier Barvaux <didier@barvaux.org>
2014-04-20 12:24:36 +0200

    Fix a few license headers.
    Thanks to symoon that reported the problem.

--- src/common/protocols/ip_numbers.h
+++ src/common/protocols/ip_numbers.h
@@ -1,3 +1,19 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 /**
  * @file   ip_numbers.h
  * @brief  Defines the IPv4 protocol numbers
@@ -8,6 +24,10 @@
  * be portable on all platforms, even the platforms that miss the
  * declarations or got different declarations, such as Microsoft Windows or
  * FreeBSD.
+ *
+ * The copyright statement of the GNU C library is:
+ *   Copyright (C) 1991-2001, 2003, 2004, 2006, 2007, 2008, 2011, 2012
+ *   Free Software Foundation, Inc.
  */
 
 #ifndef ROHC_PROTOCOLS_NUMBERS_H
