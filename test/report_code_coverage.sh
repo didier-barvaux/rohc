2302aa8a51ea159569350fc248d90039b93f5835
Didier Barvaux <didier@barvaux.org>
2014-09-07 17:07:20 +0200

    add license and copyright information to shell scripts

--- test/report_code_coverage.sh
+++ test/report_code_coverage.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 #
+# Copyright 2013,2014 Didier Barvaux
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+#
 # Create a report of code coverage with the help of zcov.
 #
 # Do not use this script directly, run configure with --enable-code-coverage,
@@ -10,6 +28,7 @@
 #   make check
 #
 # note: LANG=C and LC_ALL=C are required for zcov to work correctly
+#
 
 echo "" >&2
 
--- test/valgrind.sh
+++ test/valgrind.sh
@@ -1,9 +1,29 @@
+#!/bin/sh
+#
+# Copyright 2012,2013 Didier Barvaux
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
 #
 # file:        test_valgrind.sh
 # description: Functions to run a test with or without valgrind to check
 #              or not check for memory leaks.
 # authors:     Didier Barvaux <didier.barvaux@toulouse.viveris.com>
 #              Didier Barvaux <didier@barvaux.org>
+#
 
 
 # Run a test with valgrind to check for memory leaks
