efab8a5992559d71e35bacf9d532dd670686a28d
Didier Barvaux <didier@barvaux.org>
2014-09-07 15:33:16 +0200

    Update license and copyright of src/common/protocols/udp.h
    The src/common/protocols/udp.h file mimics the udphdr struct defined by
    netinet/udp.h from the GNU C library. It is defined in that file to be portable
    on all platforms, even the platforms that miss the declarations or got different
    declarations, such as Microsoft Windows or FreeBSD. The udphdr struct being
    trivial, the original copyrights and licenses are not kept to simplify the
    library license.

--- src/common/protocols/udp.h
+++ src/common/protocols/udp.h
@@ -1,61 +1,31 @@
-/* Copyright (C) 1991-1993,1995-1997,2004,2009 Free Software Foundation, Inc.
+/*
  * Copyright 2012 Didier Barvaux
  *
-   This file is part of the GNU C Library.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301 USA.  */
-
-/*
- * Copyright (C) 1982, 1986 Regents of the University of California.
- * All rights reserved.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 /**
  * @file   udp.h
  * @brief  Defines the UDP header
- * @author Free Software Foundation, Inc
+ * @author Didier Barvaux <didier@barvaux.org>
  *
- * This file contains a part of netinet/udp.h from the GNU C library. It is
- * copied here to be portable on all platforms, even the platforms that miss
- * the declarations or got different declarations, such as Microsoft Windows
- * or FreeBSD.
+ * This file mimics the udphdr struct defined by netinet/udp.h from the GNU C
+ * library. It is defined here to be portable on all platforms, even the
+ * platforms that miss the declarations or got different declarations, such as
+ * Microsoft Windows or FreeBSD. The udphdr struct being trivial, the original
+ * copyrights and licenses are not kept to simplify the library license.
  */
 
 #ifndef ROHC_PROTOCOLS_UDP_H
@@ -64,13 +34,13 @@
 #include <stdint.h>
 
 
-/* UDP header */
+/** The UDP header */
 struct udphdr
 {
-  uint16_t source;
-  uint16_t dest;
-  uint16_t len;
-  uint16_t check;
+  uint16_t source;  /**< The source port of the UDP header */
+  uint16_t dest;    /**< The destination port of the UDP header */
+  uint16_t len;     /**< The length (in bytes) of the UDP packet (header + payload) */
+  uint16_t check;   /**< The checksum over of the UDP header + pseudo IP header */
 } __attribute__((packed));
 
 #endif
