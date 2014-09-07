2fc56847a374edbad75e9ddf7149cba1382d5e15
Simon Paillard <spaillard@debian.org>
2014-09-07 14:36:28 +0200

    fuzzer: add missing copyright

--- app/fuzzer/start_fuzzer.sh
+++ app/fuzzer/start_fuzzer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #
+# Copyright 2013 Didier Barvaux
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
