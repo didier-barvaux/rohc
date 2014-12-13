21dd45c67031e6e10a3d320436db3827d40f70a0
Didier Barvaux <didier@barvaux.org>
2014-12-13 18:59:06 +0100

    minor: fix __attribute__ syntax

--- src/common/protocols/ip_numbers.h
+++ src/common/protocols/ip_numbers.h
@@ -94,9 +94,9 @@ enum
 
 
 bool ROHC_EXPORT rohc_is_tunneling(const uint8_t protocol)
-	__attribute((warn_unused_result, pure));
+	__attribute__((warn_unused_result, pure));
 
 bool ROHC_EXPORT rohc_is_ipv6_opt(const uint8_t protocol)
-	__attribute((warn_unused_result, pure));
+	__attribute__((warn_unused_result, pure));
 
 #endif
