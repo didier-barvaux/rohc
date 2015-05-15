1c157101a3e8c46da11a6550288569ffb51bee32
Didier Barvaux <didier@barvaux.org>
2015-05-15 12:30:14 +0200

    sniffer/fuzzer: return after raise()
    Coverity Scan reported several problems that should not happen because
    the apps raised a SIGSEGV signal just before. Since the raise() function
    might cause an immediate stop, better add a return statement after every
    raise() call.

--- app/sniffer/sniffer.c
+++ app/sniffer/sniffer.c
@@ -640,6 +640,7 @@ static void sniffer_interrupt(int signum)
 		{
 			SNIFFER_LOG(LOG_NOTICE, "no trace to print");
 			raise(SIGKILL);
+			return;
 		}
 
 		if(last_traces_first <= last_traces_last)
@@ -672,6 +673,7 @@ static void sniffer_interrupt(int signum)
 			action.sa_handler = SIG_DFL;
 			sigaction(SIGSEGV, &action, NULL);
 			raise(signum);
+			return;
 		}
 	}
 }
