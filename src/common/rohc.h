a1116b46da12f3c449c7190de13c6895ff20ad4c
Didier Barvaux <didier.barvaux@toulouse.viveris.com>
2018-03-31 16:32:51 +0200

    API: move definition of profiles in rohc/rohc_profiles.h

--- src/common/rohc.h
+++ src/common/rohc.h
@@ -32,6 +32,8 @@ extern "C"
 {
 #endif
 
+#include <rohc/rohc_profiles.h>
+
 #include <stdlib.h>
 #include <stddef.h>
 
@@ -194,79 +196,6 @@ typedef size_t rohc_cid_t;
 
 
 /*
- * ROHC profiles numbers allocated by the IANA (see 8 in the RFC 3095):
- */
-
-/**
- * @brief The different ROHC compression/decompression profiles
- *
- * If you add a new compression/decompression profile, please also add the
- * corresponding textual description in \ref rohc_get_profile_descr.
- *
- * @ingroup rohc
- *
- * @see rohc_get_profile_descr
- */
-typedef enum
-{
-	/** The ROHCv1 Uncompressed profile (RFC 3095, section 5.10) */
-	ROHC_PROFILE_UNCOMPRESSED = 0x0000,
-	/** The ROHCv1 RTP profile (RFC 3095, section 8) */
-	ROHC_PROFILE_RTP          = 0x0001,
-	/** The ROHCv1 UDP profile (RFC 3095, section 5.11) */
-	ROHC_PROFILE_UDP          = 0x0002,
-	/** The ROHCv1 ESP profile (RFC 3095, section 5.12) */
-	ROHC_PROFILE_ESP          = 0x0003,
-	/** The ROHCv1 IP-only profile (RFC 3843, section 5) */
-	ROHC_PROFILE_IP           = 0x0004,
-	/** The ROHCv1 IP/UDP/RTP Link-Layer Assisted Profile (LLA) profile
-	 *  (RFC 4362, section 6) */
-	ROHC_PROFILE_RTP_LLA      = 0x0005,
-	/** The ROHCv1 TCP profile (RFC 4996) */
-	ROHC_PROFILE_TCP          = 0x0006,
-	/** The ROHCv1 UDP-Lite/RTP profile (RFC 4019, section 7) */
-	ROHC_PROFILE_UDPLITE_RTP  = 0x0007,
-	/** The ROHCv1 UDP-Lite profile (RFC 4019, section 7) */
-	ROHC_PROFILE_UDPLITE      = 0x0008,
-
-	/** The ROHCv2 RTP/UDP/IP profile */
-	ROHCv2_PROFILE_IP_UDP_RTP     = 0x0101,
-	/** The ROHCv2 UDP/IP profile */
-	ROHCv2_PROFILE_IP_UDP         = 0x0102,
-	/** The ROHCv2 ESP/IP profile */
-	ROHCv2_PROFILE_IP_ESP         = 0x0103,
-	/** The ROHCv2 IP profile */
-	ROHCv2_PROFILE_IP             = 0x0104,
-	/** The ROHCv2 IP/UDP-Lite/RTP profile */
-	ROHCv2_PROFILE_IP_UDPLITE_RTP = 0x0107,
-	/** The ROHCv2 IP/UDP-Lite profile */
-	ROHCv2_PROFILE_IP_UDPLITE     = 0x0108,
-
-	ROHC_PROFILE_MAX              = 0x0109,
-
-} rohc_profile_t;
-
-/** Alias for PROFILE_UNCOMPRESSED ROHCv1 */
-#define ROHCv1_PROFILE_UNCOMPRESSED   ROHC_PROFILE_UNCOMPRESSED
-/** Alias for PROFILE_RTP ROHCv1 */
-#define ROHCv1_PROFILE_IP_UDP_RTP     ROHC_PROFILE_RTP
-/** Alias for PROFILE_UDP ROHCv1 */
-#define ROHCv1_PROFILE_IP_UDP         ROHC_PROFILE_UDP
-/** Alias for PROFILE_ESP ROHCv1 */
-#define ROHCv1_PROFILE_IP_ESP         ROHC_PROFILE_ESP
-/** Alias for PROFILE_IP ROHCv1 */
-#define ROHCv1_PROFILE_IP             ROHC_PROFILE_IP
-/** Alias for PROFILE_RTP_LLA ROHCv1 */
-#define ROHCv1_PROFILE_IP_UDP_RTP_LLA ROHC_PROFILE_RTP_LLA
-/** Alias for PROFILE_TCP ROHCv1 */
-#define ROHCv1_PROFILE_IP_TCP         ROHC_PROFILE_TCP
-/** Alias for PROFILE_UDPLITE_RTP ROHCv1 */
-#define ROHCv1_PROFILE_IP_UDPLITE_RTP ROHC_PROFILE_UDPLITE_RTP
-/** Alias for PROFILE_UDPLITE ROHCv1 */
-#define ROHCv1_PROFILE_IP_UDPLITE     ROHC_PROFILE_UDPLITE
-
-
-/*
  * Prototypes of public functions
  */
 
@@ -278,10 +207,6 @@ const char * ROHC_EXPORT rohc_strerror(const rohc_status_t status)
 
 const char * ROHC_EXPORT rohc_get_mode_descr(const rohc_mode_t mode)
 	__attribute__((warn_unused_result, const));
-
-const char * ROHC_EXPORT rohc_get_profile_descr(const rohc_profile_t profile)
-	__attribute__((warn_unused_result, const));
-
 
 
 #undef ROHC_EXPORT /* do not pollute outside this header */
